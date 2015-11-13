41eff87e3c93462f49fd1f583e6d3bd1bb390517
Mike Ulch <mike@norcode.com>
2015-11-13 17:23:22 -0500

    bugfix

--- picks_server/urls.py
+++ picks_server/urls.py
@@ -18,7 +18,7 @@ router.register(r'accounts', UserView, 'list')
 urlpatterns = [
     url(r'^api/', include(router.urls)),
     url(r'^rest-auth/', include('rest_auth.urls')),
-    url(r'^rest-auth/registration/', include('rest_auth.registration.urls')),4
+    url(r'^rest-auth/registration/', include('rest_auth.registration.urls')),
     url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework')),
     url(r'^admin/', include(admin.site.urls)),
 ]
